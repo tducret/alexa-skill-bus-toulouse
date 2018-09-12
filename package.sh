45e7c3c7e9760562589a0cb52cbda165592e2d35
Thibault Ducret <thibault.ducret@gmail.com>
2018-09-12 14:06:54 +0200

    Updated to create a zip with only the mandatory packages (reduce the zip size for the Lambda function)

--- package.sh
+++ package.sh
@@ -12,6 +12,7 @@ PROJECT_DIR=$(pwd)
 BUILD_DIR="build"
 SOURCE="bus_toulouse.py"
 VIRTUAL_ENV="skill"
+NEEDED_PACKAGES="ask_sdk_core ask_sdk_model"
 
 # clear out existing package
 mkdir -p ${BUILD_DIR}
@@ -20,5 +21,6 @@ rm -f "$BUILD_DIR/$PACKAGE_NAME.zip"
 # package out python module and any dependencies installed in the virtual env
 zip -r9 "$BUILD_DIR/$PACKAGE_NAME.zip" "$SOURCE"
 cd $VIRTUAL_ENV/lib/python3.7/site-packages
-zip -r9 "$PROJECT_DIR/$BUILD_DIR/$PACKAGE_NAME.zip" .
+find . \( -name __pycache__ -o -name "*.pyc" \) -delete
+zip -r9 "$PROJECT_DIR/$BUILD_DIR/$PACKAGE_NAME.zip" $NEEDED_PACKAGES
 cd ${PROJECT_DIR}
